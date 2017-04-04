427f412917c31f30465d55b8de462b3e3633e6c8
Gordondalos <gordondalos@gmail.com>
2017-04-04 22:51:55 +0600

    Синхронные экшены в redux

---
+++ redux.md
@@ -0,0 +1,38 @@
+import { createStore } from 'redux';
+
+function playlist(state = [], action) {
+	if (action.type === 'ADD_TRACK') {
+		return [
+			...state,
+			action.payload
+		]
+	}
+	return state;
+}
+
+const addTrackBtn = document.getElementById('addTrack');
+const input = document.querySelector('.trackInput');
+const list = document.querySelector('ul');
+
+const store = createStore ( playlist );
+
+// подписываемся на изменения в сторе
+store.subscribe(()=>{
+	console.log('subscribe',store.getState());
+	list.innerHTML='';
+	input.value = '';
+	store.getState().forEach(track => {
+		const li = document.createElement('li');
+		li.textContent = track;
+		list.appendChild(li);
+	})
+});
+
+// изменяем значения в сторе
+store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+
+
+addTrackBtn.addEventListener('click', ()=>{
+	const trackName = input.value;
+	store.dispatch({type: 'ADD_TRACK', payload: trackName});
+});
--- src/index.js
+++ src/index.js
@@ -1,48 +1,66 @@
+import  React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import App from './App';
+import './index.css';
 
-function playlist(state = [], action) {
-	if (action.type === 'ADD_TRACK') {
-		return [
-			...state,
-			action.payload
-		]
-	}
-	return state;
-}
-
-const addTrackBtn = document.getElementById('addTrack');
-const input = document.querySelector('.trackInput');
-const list = document.querySelector('ul');
-
-const store = createStore ( playlist );
-
-// подписываемся на изменения в сторе
-store.subscribe(()=>{
-	console.log('subscribe',store.getState());
-	list.innerHTML='';
-	input.value = '';
-	store.getState().forEach(track => {
-		const li = document.createElement('li');
-		li.textContent = track;
-		list.appendChild(li);
-	})
-});
-
-// изменяем значения в сторе
-store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+import reduser from './reducers';
 
 
-addTrackBtn.addEventListener('click', ()=>{
-	const trackName = input.value;
-	store.dispatch({type: 'ADD_TRACK', payload: trackName});
-});
 
 
-// ReactDOM.render (
-// 	<Router history={hashHistory}>
-// 		<Route path="/" component={App}/>
-// 	</Router>,
+// делаем стор, сразу подключаем его к хрому как расширение вот этим __REDUX_DEVTOOLS_EXTENSION__
+const store = createStore ( reduser, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+
+
+ReactDom.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
+
+// import { createStore } from 'redux';
 //
-// 	document.getElementById ( 'root' )
-// );
+// function playlist(state = [], action) {
+// 	if (action.type === 'ADD_TRACK') {
+// 		return [
+// 			...state,
+// 			action.payload
+// 		]
+// 	}
+// 	return state;
+// }
+//
+// const addTrackBtn = document.getElementById('addTrack');
+// const input = document.querySelector('.trackInput');
+// const list = document.querySelector('ul');
+//
+// const store = createStore ( playlist );
+//
+// // подписываемся на изменения в сторе
+// store.subscribe(()=>{
+// 	console.log('subscribe',store.getState());
+// 	list.innerHTML='';
+// 	input.value = '';
+// 	store.getState().forEach(track => {
+// 		const li = document.createElement('li');
+// 		li.textContent = track;
+// 		list.appendChild(li);
+// 	})
+// });
+//
+// // изменяем значения в сторе
+// store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+//
+//
+// addTrackBtn.addEventListener('click', ()=>{
+// 	const trackName = input.value;
+// 	store.dispatch({type: 'ADD_TRACK', payload: trackName});
+// });
+
+
+
 
