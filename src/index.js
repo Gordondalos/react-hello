b61ce3e21f7f6592a7d64f04f35c45d304a536a0
Gordondalos <gordondalos@gmail.com>
2017-04-04 20:02:03 +0600

    Попробовали редукс, подключаем редукс к реакту

--- src/index.js
+++ src/index.js
@@ -10,18 +10,32 @@ function playlist(state = [], action) {
 	return state;
 }
 
+const addTrackBtn = document.getElementById('addTrack');
+const input = document.querySelector('.trackInput');
+const list = document.querySelector('ul');
+
 const store = createStore ( playlist );
 
 // подписываемся на изменения в сторе
 store.subscribe(()=>{
 	console.log('subscribe',store.getState());
+	list.innerHTML='';
+	input.value = '';
+	store.getState().forEach(track => {
+		const li = document.createElement('li');
+		li.textContent = track;
+		list.appendChild(li);
+	})
 });
 
 // изменяем значения в сторе
 store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
-store.dispatch({type: 'ADD_TRACK', payload: 'хо хо хо'});
 
 
+addTrackBtn.addEventListener('click', ()=>{
+	const trackName = input.value;
+	store.dispatch({type: 'ADD_TRACK', payload: trackName});
+});
 
 
 // ReactDOM.render (
