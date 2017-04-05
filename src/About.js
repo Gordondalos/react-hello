4fc89a0e631cd3ecf03f492ce82742cd28456b88
Gordondalos <gordondalos@gmail.com>
2017-04-05 14:59:21 +0600

    Использование библиотеки афродита для изоляции ксс в приложении на ангуляр

--- src/About.js
+++ src/About.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 
+import {css} from 'aphrodite/no-important';
+import styles from './appStyles';
+
 class About extends Component {
 	render () {
 		return (
-			<div>
+			<div className={css(styles.container)}>
 				Крутое приложение
 			</div>
 		)
